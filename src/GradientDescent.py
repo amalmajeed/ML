8e1fe77130daddeef216739a1bd6c03e211d714e
Amal Majeed <amalmajeed7@gmail.com>
2019-07-26 07:59:18 +0530

    Functionized and scaled down MSE

--- src/GradientDescent.py
+++ src/GradientDescent.py
@@ -8,10 +8,17 @@ GLOBAL_MIN = np.inf
 THETA0 = 0
 THETA1 = 0
 
+def op_map(optrain):
+	op = [i*3 for i in optrain]
+	return op
+
 def hypothesis(th0,th1,x):
 	return(th0+th1*x)
 
 def cost_function(th0,th1,ip,op):
+	"""
+			AVERAGE MEAN SQUARED ERROR COST FUNCTION
+	"""
 	global GLOBAL_MIN,THETA1,THETA0
 	pred_err = []
 	for i,val in enumerate(ip):
@@ -23,71 +30,42 @@ def cost_function(th0,th1,ip,op):
 		THETA1 = th1
 	return MSE
 
+def GradientDescent(theta0,theta1,learning_rate,input_train,label_train):
+	MSE_train = []
+	parameter_train = []
+	for i in range(0,len(input_train)):
+		htheta = hypothesis(theta0,theta1,input_train[i])
+		y = label_train[i]
+		e = y - htheta
+		theta0 = theta0 + learning_rate*e # Bias Update
+		theta1 = theta1 + learning_rate*e*input_train[i] # Weight Update
+		parameter_train.append((theta1,theta0))
+		MSE_train.append(cost_function(theta0,theta1,input_train,label_train))
+	return MSE_train,parameter_train
 
-input_train = np.array(range(1,50))
-label_train = np.array(range(1,50))
-#predict_train = np.array([])
-MSE_train = []
-parameter_train = []
-#theta0_error_train = []
-#theta1_error_train = []
 
-theta0 , theta1 = 1 , 0.5
+def PlotCostFunction(x,y,xlim1,xlim2,ylim1,ylim2):
+	plt.xlim(xlim1,xlim2)
+	plt.ylim(ylim1,ylim2)
+	plt.gca().set_aspect('equal', adjustable='box')
+	plt.plot(x,y,'ro')
+	plt.show()
 
-learning_rate = 0.004
 
-# Hypothesis
-
-# predict_train = theta0 + theta1*input_train
-# error_train = label_train - predict_train
-# jtheta = sum([i**2 for i in error_train])
-
-for i in range(0,len(input_train)):
-	htheta = hypothesis(theta0,theta1,input_train[i])
-	y = label_train[i]
-	e = y - htheta
-	theta0 = theta0 + learning_rate*e
-	theta1 = theta1 + learning_rate*e*input_train[i]
-	parameter_train.append((theta1,theta0))
-	MSE_train.append(cost_function(theta0,theta1,input_train,label_train))
-
-print(input_train)
-#print(parameter_train)
-theta1_train = [i for i,j in parameter_train]
-#print(MSE_train)
-print(f"GLOBAL_MIN : {GLOBAL_MIN}\n THETA0 : {THETA0}\n THETA1 : {THETA1}\n")
-
-#Error = sum( [ (i-j)**2 for i,j in zip(predict_train, label_train)]) # ERROR = Sum of Squares of 
-
-#Cost = 0.5 * Error # COST FUNCTION CALCULATION
+def __main__():
+	input_train = np.array(range(1,100))
+	label_train = np.array(op_map(range(1,100)))
+	theta0 , theta1 = 1 , 0.5
+	learning_rate = 0.000025
+	print(f"Input Dataset : {input_train}")
+	MSE , PARAM = GradientDescent(theta0,theta1,learning_rate,input_train,label_train)
+	theta1_train = [i for i,j in PARAM]
+	print(f"GLOBAL_MIN : {GLOBAL_MIN}\n THETA0 : {THETA0}\n THETA1 : {THETA1}\n")
+	print(f"MEAN SQUARED ERRORS : {MSE}\n THETA1 : {theta1_train}")
+	MSE_SC = [i/2 for i in MSE]
+	PlotCostFunction(theta1_train,MSE_SC,xlim1=-5,xlim2=5,ylim1=0,ylim2=10)
 
 
 
-
-
-
-
-
-# boundary = [(i,j) for i,j in zip(range(-50,51),[m**2 for m in range(-50,51)])]
-# x = [i for i,j in boundary]
-# y = [j for i,j in boundary]
-
-
-
-
-
-
-
-
-
-
-
-
-plt.xlim(0, 60)
-plt.ylim(0, 100)
-plt.gca().set_aspect('equal', adjustable='box')
-
-plt.plot(range(1,50),MSE_train)
-plt.show()
-
-
+if __name__ =="__main__":
+	__main__()
